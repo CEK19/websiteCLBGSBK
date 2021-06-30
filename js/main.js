906f52b43d5e6c68a33253292dc26fbf12f25627
9D <quocdungngole@gmail.com>
2021-07-01 00:14:28 +0700

    Update hình ảnh nhẹ hơn, thêm favicon

--- js/main.js
+++ js/main.js
@@ -13,6 +13,7 @@
             $('.back-to-top').fadeOut('slow');
         }
     });
+    
     $('.back-to-top').click(function () {
         $('html, body').animate({scrollTop: 0}, 1500, 'easeInOutExpo');
         return false;
